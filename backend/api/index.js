7a2dd3101067c8723d1d800436fa34298fa9f4e0
ShotaLaabb <shotikoslabadzes1@gmail.com>
2025-12-04 17:20:58 +0400

    make app serverless

---
+++ backend/api/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("../config/db");
+const authRoutes = require("../routes/authRoutes");
+const categoryRoutes = require("../routes/categoryRoutes");
+const financeRoutes = require("../routes/financeRoutes");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/finances", financeRoutes);
+
+connectDB();
+
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,5 @@
 require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const app = require("./api/index"); // Import Express app
 
-const authRoutes = require("./routes/authRoutes");
-const financeRoutes = require("./routes/financeRoutes");
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected!"))
-  .catch((err) => console.error(err));
-
-app.use("/api/auth", authRoutes);
-app.use("/api/finances", financeRoutes);
-
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
