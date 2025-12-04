bc065980e365adb2a964b74c7638b372652b0331
ShotaLaabb <shotikoslabadzes1@gmail.com>
2025-12-04 16:59:12 +0400

    fix vercel

--- backend/server.js
+++ backend/server.js
@@ -7,20 +7,15 @@ const authRoutes = require("./routes/authRoutes");
 const financeRoutes = require("./routes/financeRoutes");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected!"))
-  .catch((err) => console.error("MongoDB error:", err));
+  .catch((err) => console.error(err));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/finances", financeRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Finance API is running!");
-});
 
 module.exports = app;
