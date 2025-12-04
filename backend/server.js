f05bce07a183471e0e590c3b1f851ca72618159d
ShotaLaabb <shotikoslabadzes1@gmail.com>
2025-12-04 17:34:24 +0400

    .

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,23 @@
 require("dotenv").config();
-const app = require("./api/index"); // Import Express app
+const cors = require("cors");
+const express = require("express");
+const serverless = require("serverless-http");
+const connectDB = require("./config/db");
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const authRoutes = require("./routes/authRoutes");
+const financeRoutes = require("./routes/financeRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/categories", categoryRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/finances", financeRoutes);
+
+connectDB()
+  .then(() => console.log("MongoDB connected!"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+module.exports = serverless(app);
