a913100b0a7f9a03ae7b69fe2c96e0e461a4bd52
ShotaLaabb <shotikoslabadzes1@gmail.com>
2025-12-04 17:47:24 +0400

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,26 @@
 const mongoose = require("mongoose");
 
 let cached = global.mongoose;
+
 if (!cached) cached = global.mongoose = { conn: null, promise: null };
 
 async function connect() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI);
+    cached.promise = mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
   }
 
-  cached.conn = await cached.promise;
-  return cached.conn;
+  try {
+    cached.conn = await cached.promise;
+    return cached.conn;
+  } catch (err) {
+    cached.promise = null;
+    throw err;
+  }
 }
 
 module.exports = connect;
