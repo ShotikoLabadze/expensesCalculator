d408eec8e53945a827c97d5bc64993e74473bdf9
ShotaLaabb <shotikoslabadzes1@gmail.com>
2025-12-04 16:46:31 +0400

    fix server js, add vercel

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,20 @@ const authRoutes = require("./routes/authRoutes");
 const financeRoutes = require("./routes/financeRoutes");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected!"))
-  .catch((err) => console.error(err));
+  .catch((err) => console.error("MongoDB error:", err));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/finances", financeRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("Finance API is running!");
+});
+
+module.exports = app;
